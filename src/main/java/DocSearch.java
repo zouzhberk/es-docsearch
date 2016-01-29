d702e307c299e7eeb0e8992e4eed592a05b57832
zouzhberk <zouzhberk@gmail.com>
2016-01-29 16:14:38 +0800

    add base64 util

--- src/main/java/DocSearch.java
+++ src/main/java/DocSearch.java
@@ -1,7 +1,31 @@
+import org.elasticsearch.common.io.Streams;
+import sun.misc.BASE64Encoder;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
 /**
  * Created by berk on 1/29/16.
  */
 public class DocSearch {
 
+
+    public static void main(String[] args) throws IOException {
+
+        BASE64Encoder encoder = new BASE64Encoder();
+
+        Path path = Paths.get("/home/berk/IdeaProjects/es-docsearch/README.md");
+        InputStream buffer1 = Files.newInputStream(path);
+        long size = Files.size(path);
+
+        //BufferedReader aa = new BufferedReader(buffer1);
+
+        byte[] bytes = new byte[Long.valueOf(size).intValue()];
+        Streams.readFully(buffer1, bytes);
+        Base64.getEncoder().encode(bytes);
+    }
 
 }
---
+++ src/main/java/utils/Base64Utils.java
@@ -0,0 +1,46 @@
+package utils;
+
+import org.elasticsearch.common.io.Streams;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+/**
+ * Created by berk on 1/29/16.
+ */
+final public class Base64Utils {
+
+
+    public static byte[] encodeToBase64(Path path) throws IOException {
+        BASE64Encoder encoder = new BASE64Encoder();
+        InputStream buffer1 = Files.newInputStream(path);
+        long size = Files.size(path);
+        byte[] bytes = new byte[Long.valueOf(size).intValue()];
+
+        Streams.readFully(buffer1, bytes);
+        return Base64.getEncoder().encode(bytes);
+    }
+
+    public static byte[] decodeToBytes(String base64String) throws IOException {
+        BASE64Decoder decoder = new BASE64Decoder();
+        return decoder.decodeBuffer(base64String);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("/home/berk/IdeaProjects/es-docsearch/README.md");
+        String base64str = new String(encodeToBase64(path));
+
+        System.out.println(base64str);
+
+        byte[] output = decodeToBytes(base64str);
+        System.out.println(new String(output));
+
+    }
+
+}
